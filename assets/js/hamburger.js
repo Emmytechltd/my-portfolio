41aa4e15444572adf3f9e64466886662873fcd7a
unknown <you@example.com>
2021-05-26 05:30:36 +0100

    first commit

---
+++ assets/js/hamburger.js
@@ -0,0 +1,8 @@
+const navItemContainer = document.getElementById(nav-items-wrapper)
+
+
+
+// function that open and close the navbar//
+function toggleNavbar(){
+    navItemContainer.classList.toggle("open-nav-items")
+} 
